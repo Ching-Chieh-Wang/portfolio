9445626f3c3ed7dd3a31131b70fb65eb097bc8f4
Ching-Chieh-Wang <immactavish@gmail.com>
2025-09-26 12:01:01 -0400

    fix: reorder routes in App component for consistency

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<Navigate to="/"/>} />
-          <Route path="/project/leximind" element={<LeximindDetail />} />
+          <Route path="project" element={<Projects />} />
+          <Route path="about" element={<About />} />
+          <Route path="project/leximind" element={<LeximindDetail />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     </Router>
   );
