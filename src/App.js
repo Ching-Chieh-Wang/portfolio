e467737248036b1963a395bf84f7bcb0a441c964
Ching-Chieh-Wang <immactavish@gmail.com>
2025-09-26 11:47:22 -0400

    fix: set basename for Router in App component

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
 
   return (
-    <Router>
+    <Router basename="/portfolio">
         <Navbar />
         <ScrollToTop />
         <Routes>
